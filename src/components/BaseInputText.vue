b26a752416ba41513dbcf3a0da737e5eb13d6990
Ediven <hufancool@outlook.com>
2018-04-11 19:32:29 +0800

    Add template

---
+++ src/components/BaseInputText.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    type="text"
+    class="input"
+    :value="value"
+
+    @input="$emit('input', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style  scoped>
+
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid #32485F;
+}
+</style>
